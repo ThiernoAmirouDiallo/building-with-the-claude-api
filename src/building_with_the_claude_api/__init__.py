ae0923efc671458f8e3c889819113c574351fe84
thiernoamiroudiallo <thiernoamiroud@gmail.com>
2026-09-01 23:17:15 -0400

    adding system prompt support

--- src/building_with_the_claude_api/__init__.py
+++ src/building_with_the_claude_api/__init__.py
@@ -10,12 +10,18 @@ def add_assistant_message(messages, text):
     assistant_message = {"role": "assistant", "content": text}
     messages.append(assistant_message)
 
-def chat(messages, client):
-    message = client.messages.create(
-        model="claude-haiku-4-5-20251001",
-        max_tokens=1024,
-        messages=messages
-    )
+def chat(messages, client, system_prompt=None):
+    request = {
+        "model": "claude-haiku-4-5-20251001",
+        "max_tokens":1024,
+        "messages": messages
+    };
+
+    if system_prompt is not None:
+        request["system"] = system_prompt
+
+    message = client.messages.create(**request)
+
     return message.content[0].text
 
 def main() -> None:
@@ -25,14 +31,16 @@ def main() -> None:
     client = Anthropic()
     # client = Anthropic(api_key=os.environ.get("ANTHROPIC_API_KEY"))
 
+    system_prompt = "Explain to a non STEM background person so be non technical."
     messages = []
     add_user_message(messages=messages, text="what is ML vs AI engineering. Only one sentence")
-    response = chat(messages=messages, client=client)
+    response = chat(messages=messages, client=client, system_prompt=system_prompt)
     add_assistant_message(messages=messages, text=response)
     print(response)
+    print("---")
 
     add_user_message(messages=messages, text="which one has a higher bar to braking into the field. Only one sentence")
-    response = chat(messages=messages, client=client)
+    response = chat(messages=messages, client=client, system_prompt=system_prompt)
     add_assistant_message(messages=messages, text=response)
     print(response)
     
